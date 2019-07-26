8531ba8d7bb720296ab3e1ff2066d47145edc175
Marzena <marzena.szopinska090@gmail.com>
2019-07-26 13:17:07 +0200

    styled the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,10 @@ import React from 'react';
 const HomePage = () => {
     return (
         <main>
-        home page
+            <div className='intro'>
+                <h1 className='name'>Marzena Szopinska</h1>
+                <h3>Junior Front End Developer</h3>
+            </div>
         </main>
     );
 }
