a0b48d2432ba4beaaad4ca185756dc12ac030a14
Marzena <marzena.szopinska090@gmail.com>
2019-08-02 12:21:32 +0200

    added typing animation to the home page intro

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,10 @@ const HomePage = () => {
     return (
         <main>
             <div className='intro'>
-                <h1 className='name'>ALex Smitche</h1>
-                <h3>Travel the world with me!</h3>
+                  <h1 className='name'>ALex Smitche</h1>
+                  <div className='blink'>
+                    <h3 className='typing-effect'>Travel the world with me!</h3>
+                  </div>
                 <div className='social-icons'>
                   <i class="fab fa-facebook-square"></i>
                   <i class="fab fa-twitter-square"></i>
