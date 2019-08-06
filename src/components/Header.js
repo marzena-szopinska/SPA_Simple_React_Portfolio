0ca495e881a83091ddf99612415dba5e8e585d77
Marzena <marzena.szopinska090@gmail.com>
2019-08-06 18:35:07 +0200

    working on a new navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
+// <i class="far fa-times-circle"></i>
+// <i className="fas fa-bars navigation__hamburger"></i>
+// <div className='navigation'>
+//     <div className='navigation__box'>
+
+//     </div>
+// </div>
+
 const Header = () => {
     return (
+      <div>
+
+        <div className='navigation'>
+          <input type='checkbox' className='navigation__checkbox' id='navi-toggle' />
+          <label for='navi-toggle' className='navigation__button'>MENU</label>
+          <div className='navigation__background'>&nbsp;</div>
+
+          <nav className='navigation__nav'>
+                <ul className='navigation__list'>
+                    <li className='navigation__item'><NavLink to='/' className='navigation__link' activeClassName='is-active' exact={true}>Home</NavLink></li>
+                    <li className='navigation__item'><NavLink to='/portfolio' className='navigation__link' activeClassName='is-active'>Portfolio</NavLink></li>
+                    <li className='navigation__item'><NavLink to='/contact' className='navigation__link' activeClassName='is-active'>Contact</NavLink></li>
+                </ul>
+          </nav>
+        </div>
         <header>
             <div className='container'>
-                <ul className='nav'>
-                    <li><NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink></li>
-                    <li><NavLink to='/portfolio' activeClassName='is-active'>Portfolio</NavLink></li>
-                    <li><NavLink to='/contact' activeClassName='is-active'>Contact</NavLink></li>
-                </ul>
+
             </div>
         </header>
+
+        </div>
     );
 }
 
